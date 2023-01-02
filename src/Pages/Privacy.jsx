0a54824bf8b9094729ad598815beccec6fd08822
Simon Robinson <70141158+Chosen2730@users.noreply.github.com>
2023-01-02 16:51:19 +0100

    completed privacy policy page

--- src/Pages/Privacy.jsx
+++ src/Pages/Privacy.jsx
@@ -6,7 +6,7 @@ const Privacy = () => {
     window.scrollTo({ top: 0 });
   }, []);
   return (
-    <div className='max-w-6xl mx-auto p-6'>
+    <div className='max-w-4xl mx-auto p-6 text-gray-700'>
       <h1 className='text-2xl uppercase font-bold'>Privacy Policy</h1>
       <h2 className='text-base mt-5 my-3 font-bold'>
         COLLECTING PERSONAL INFORMATION
@@ -27,22 +27,70 @@ const Privacy = () => {
         <h2 className='text-base mt-5 my-3 uppercase font-bold'>
           Device information
         </h2>
-        <p className='my-2'>
-          <strong>Examples of Personal Information collected: </strong> version
-          of web browser, IP address, time zone, cookie information, what sites
-          or products you view, search terms, and how you interact with the
-          Site.
-        </p>
-        <p className='my-4'>
-          <strong>Purpose of collection:</strong> to load the Site accurately
-          for you, and to perform analytics on Site usage to optimize our Site.
-        </p>
-        <p className='my-4'>
-          <strong>Source of collection:</strong> Collected automatically when
-          you access our Site using cookies, log files, web beacons, tags, or
-          pixels. Disclosure for a business purpose: shared with our processor
-          Shopify.
-        </p>
+        <div className='ml-4'>
+          <li className='my-4 list-disc'>
+            <strong>Examples of Personal Information collected: </strong>{" "}
+            version of web browser, IP address, time zone, cookie information,
+            what sites or products you view, search terms, and how you interact
+            with the Site.
+          </li>
+          <li className='my-4 list-disc'>
+            <strong>Purpose of collection:</strong> to load the Site accurately
+            for you, and to perform analytics on Site usage to optimize our
+            Site.
+          </li>
+          <li className='my-4 list-disc'>
+            <strong>Source of collection:</strong> Collected automatically when
+            you access our Site using cookies, log files, web beacons, tags, or
+            pixels.
+          </li>
+          <li className='my-4 list-disc'>
+            <strong>Disclosure for a business purpose:</strong> shared with our
+            processor Shopify.
+          </li>
+        </div>
+        <h2 className='text-base mt-5 my-3 uppercase font-bold'>
+          Order information
+        </h2>
+        <div className='ml-4'>
+          <li className='my-4 list-disc'>
+            <strong>Examples of Personal Information collected: </strong>name,
+            billing address, shipping address, payment information (including
+            credit card numbers, email address, and phone number)
+          </li>
+          <li className='my-4 list-disc'>
+            <strong>Purpose of collection:</strong> to provide products or
+            services to you to fulfil our contract, to process your payment
+            information, arrange for shipping, and provide you with invoices
+            and/or order confirmations, communicate with you, screen our orders
+            for potential risk or fraud, and when in line with the preferences
+            you have shared with us, provide you with information or advertising
+            relating to our products or services.
+          </li>
+          <li className='my-4 list-disc'>
+            <strong>Source of collection:</strong> collected from you.
+          </li>
+          <li className='my-4 list-disc'>
+            <strong>Disclosure for a business purpose:</strong> shared with our
+            processor Shopify.
+          </li>
+        </div>
+        <h2 className='text-base mt-5 my-3 uppercase font-bold'>
+          Customer support information
+        </h2>
+        <div className='ml-4'>
+          <li className='my-4 list-disc'>
+            <strong>Examples of Personal Information collected: </strong>email
+            addresses and phone numbers
+          </li>
+          <li className='my-4 list-disc'>
+            <strong>Purpose of collection:</strong> to provide customer support.
+          </li>
+          <li className='my-4 list-disc'>
+            <strong>Source of collection:</strong> collected from you.
+          </li>
+        </div>
+
         <h2 className='text-base mt-5 my-3 uppercase font-bold'>Minors</h2>
         <p className='my-4'>
           The Site is not intended for individuals under the age of 13. We do
@@ -51,6 +99,191 @@ const Privacy = () => {
           Personal Information, please contact us at the address below to
           request deletion.
         </p>
+        <h2 className='text-base mt-5 my-3 uppercase font-bold'>Minors</h2>
+        <p className='my-2'>
+          The Site is not intended for individuals under the age of 13. We do
+          not intentionally collect Personal Information from children. If you
+          are the parent or guardian and believe your child has provided us with
+          Personal Information, please contact us at the address below to
+          request deletion.
+        </p>
+        <h2 className='text-base mt-5 my-3 uppercase font-bold'>
+          Sharing Personal Information
+        </h2>
+        <p className='my-2'>
+          We share your Personal Information with service providers to help us
+          provide our services and fulfil our contracts with you, as described
+          above. For example
+        </p>
+        <div className='ml-4'>
+          <li className='list-disc'>
+            We use Shopify to power our online store. You can read more about
+            how Shopify uses your Personal Information here:
+            https://www.shopify.com/legal/privacy.
+          </li>
+          <li className='list-disc'>
+            We may share your Personal Information to comply with applicable
+            laws and regulations, to respond to a subpoena, search warrant or
+            other lawful request for information we receive, or to otherwise
+            protect our rights.
+          </li>
+        </div>
+        <h2 className='text-base mt-5 my-3 uppercase font-bold'>
+          BEHAVIORAL ADVERTISING
+        </h2>
+        <p className='my-2'>
+          As described above, we use your Personal Information to provide you
+          with targeted advertisements or marketing communications we believe
+          may be of interest to you. For example:
+        </p>
+        <li className='list-disc ml-4'>
+          We use Google Analytics to help us understand how our customers use
+          the Site. You can read more about how Google uses your Personal
+          Information here: https://policies.google.com/privacy?hl=en.You can
+          also opt-out of Google Analytics here:
+          https://tools.google.com/dlpage/gaoptout.
+        </li>
+        <p className='my-2'>
+          For more information about how targeted advertising works, you can
+          visit the Network Advertising Initiative’s (“NAI”) educational page at
+          http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work.
+        </p>
+        <p className='my-2'>You can opt out of targeted advertising by:</p>
+        <div className='ml-4'>
+          <li className='my-2 list-disc'>
+            FACEBOOK - https://www.facebook.com/settings/?tab=ads
+          </li>
+          <li className='my-2 list-disc'>
+            GOOGLE - https://www.google.com/settings/ads/anonymous
+          </li>
+          <li className='my-2 list-disc'>
+            BING -
+            https://advertise.bingads.microsoft.com/en-us/resources/policies/personalized-ads
+          </li>
+        </div>
+        <p className='my-4'>
+          Additionally, you can opt out of some of these services by visiting
+          the Digital Advertising Alliance’s opt-out portal at:
+          http://optout.aboutads.info/.
+        </p>
+
+        <h2 className='text-base mt-5 my-3 uppercase font-bold'>
+          Text Marketing and notifications:
+        </h2>
+
+        <p className='my-4'>
+          By entering your phone number in the checkout and initializing a
+          purchase, subscribing via our subscription form or a keyword, you
+          agree that we may send you text notifications (for your order,
+          including abandoned cart reminders) and text marketing offers. Text
+          marketing messages will not exceed 10 a month. You acknowledge that
+          consent is not a condition for any purchase. <br />
+          <br /> If you wish to unsubscribe from receiving text marketing
+          messages and notifications reply with STOP to any mobile message sent
+          from us or use the unsubscribe link we provided you within any of our
+          messages. <br /> <br /> You understand and agree that alternative
+          methods of opting out, such as using alternative words or requests
+          will not be accounted as a reasonable means of opting out. Message and
+          data rates may apply. For any questions please text HELP to the number
+          you received the messages from. You can also contact us for more
+          information. If you wish to opt out please follow the procedures
+          above.
+        </p>
+
+        <h2 className='text-base mt-5 my-3 uppercase font-bold'>
+          Using Personal Information
+        </h2>
+        <p className='my-2'>
+          We use your personal Information to provide our services to you, which
+          includes: offering products for sale, processing payments, shipping
+          and fulfillment of your order, and keeping you up to date on new
+          products, services, and offers.
+        </p>
+
+        <h2 className='text-base mt-5 my-3 uppercase font-bold'>
+          LAWFUL BASIS
+        </h2>
+        <p className='my-2'>
+          Pursuant to the General Data Protection Regulation (“GDPR”), if you
+          are a resident of the European Economic Area (“EEA”), we process your
+          personal information under the following lawful bases:
+        </p>
+        <div className='ml-4'>
+          <li className='list-disc'>Your consent</li>
+          <li className='list-disc'>
+            The performance of the contract between you and the Site
+          </li>
+          <li className='list-disc'>Compliance with our legal obligations</li>
+          <li className='list-disc'>To protect your vital interests</li>
+          <li className='list-disc'>
+            To perform a task carried out in the public interest
+          </li>
+          <li className='list-disc'>
+            For our legitimate interests, which do not override your fundamental
+            rights and freedoms.
+          </li>
+        </div>
+        <h2 className='text-base mt-5 my-3 uppercase font-bold'>RETENTION</h2>
+        <p className='my-2'>
+          When you place an order through the Site, we will retain your Personal
+          Information for our records unless and until you ask us to erase this
+          information. For more information on your right of erasure, please see
+          the ‘Your rights’ section below.
+        </p>
+
+        <h2 className='text-base mt-5 my-3 uppercase font-bold'>
+          AUTOMATIC DECISION-MAKING
+        </h2>
+        <p className='my-2'>
+          If you are a resident of the EEA, you have the right to object to
+          processing based solely on automated decision-making (which includes
+          profiling), when that decision-making has a legal effect on you or
+          otherwise significantly affects you. <br />
+          <br /> We do not engage in fully automated decision-making that has a
+          legal or otherwise significant effect using customer data. <br />
+          <br /> Our processor Shopify uses limited automated decision-making to
+          prevent fraud that does not have a legal or otherwise significant
+          effect on you. <br />
+          <br /> Services that include elements of automated decision-making
+          include:
+        </p>
+        <div className='ml-4'>
+          <li className='list-disc my-2'>
+            Temporary deny list of IP addresses associated with repeated failed
+            transactions. This denies list persists for a small number of hours.
+          </li>
+          <li className='list-disc my-2'>
+            Temporary denialist of credit cards associated with deny listed IP
+            addresses. This denialist persists for a small number of days.
+          </li>
+        </div>
+
+        <h2 className='text-base mt-5 my-3 uppercase font-bold'>Your Rights</h2>
+        <h2 className='font-bold'>GDPR</h2>
+        <p className='my-2'>
+          If you are a resident of the EEA, you have the right to access the
+          Personal Information we hold about you, to port it to a new service,
+          and to ask that your Personal Information be corrected, updated, or
+          erased. If you would like to exercise these rights, please contact us
+          through the contact information below. <br />
+          <br /> Your Personal Information will be initially processed in
+          Ireland and then will be transferred outside of Europe for storage and
+          further processing, including to Canada and the United States. For
+          more information on how data transfers comply with the GDPR, see
+          Shopify’s GDPR Whitepaper:
+          https://help.shopify.com/en/manual/your-account/privacy/GDPR.
+        </p>
+        <h2 className='font-bold'>CCPA</h2>
+        <p className='my-2'>
+          If you are a resident of California, you have the right to access the
+          Personal Information we hold about you (also known as the ‘Right to
+          Know’), to port it to a new service, and to ask that your Personal
+          Information be corrected, updated, or erased. <br />
+          <br /> If you would like to exercise these rights, please contact us
+          through the contact information below. If you would like to designate
+          an authorized agent to submit these requests on your behalf, please
+          contact us at the address below.
+        </p>
         <h2 className='text-base mt-5 my-3 uppercase font-bold'>Cookies</h2>
         <p className='my-2'>
           A cookie is a small amount of information that’s downloaded to your
@@ -58,11 +291,43 @@ const Privacy = () => {
           different cookies, including functional, performance, advertising, and
           social media or content cookies. Cookies make your browsing experience
           better by allowing the website to remember your actions and
-          preferences (such as login and region selection). This means you don’t
-          have to re-enter this information each time you return to the site or
-          browse from one page to another. Cookies also provide information on
-          how people use the website, for instance whether it’s their first time
-          visiting or if they are a frequent visitor.
+          preferences (such as login and region selection). <br />
+          <br /> This means you don’t have to re-enter this information each
+          time you return to the site or browse from one page to another.
+          Cookies also provide information on how people use the website, for
+          instance whether it’s their first time visiting or if they are a
+          frequent visitor. <br />
+          <br /> The length of time that a cookie remains on your computer or
+          mobile device depends on whether it is a “persistent” or “session”
+          cookie. Session cookies last until you stop browsing and persistent
+          cookies last until they expire or are deleted. Most of the cookies we
+          use are persistent and will expire between 30 minutes and two years
+          from the date they are downloaded to your device. <br />
+          <br />
+          You can control and manage cookies in various ways. Please keep in
+          mind that removing or blocking cookies can negatively impact your user
+          experience and parts of our website may no longer be fully accessible.
+          Most browsers automatically accept cookies, but you can choose whether
+          or not to accept cookies through your browser controls, often found in
+          your browser’s “Tools” or “Preferences” menu.
+          <br />
+          <br /> For more information on how to modify your browser settings or
+          how to block, manage or filter cookies can be found in your browser’s
+          help file or through such sites as www.allaboutcookies.org.
+          Additionally, please note that blocking cookies may not completely
+          prevent how we share information with third parties such as our
+          advertising partners.
+          <br />
+          <br /> To exercise your rights or opt-out of certain uses of your
+          information by these parties, please follow the instructions in the
+          “Behavioral Advertising” section above.
+        </p>
+        <h2 className='text-base my-3 uppercase font-bold'>DO NOT TRACK</h2>
+        <p className='my-2'>
+          Please note that because there is no consistent industry understanding
+          of how to respond to “Do Not Track” signals, we do not alter our data
+          collection and usage practices when we detect such a signal from your
+          browser.
         </p>
         <h2 className='text-base my-3 uppercase font-bold'>CHANGES</h2>
         <p className='my-2'>
